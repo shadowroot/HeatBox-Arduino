b77c33a60a5722850af34da290404dbff74c77d1
jonny <jonny@p2psw.xyz>
2021-12-22 20:53:12 +0100

    Untested I2C support

--- src/conf.h
+++ src/conf.h
@@ -2,4 +2,12 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
+//Comment or delete to disable I2C display support
+// Board	SDA	SCL
+// Arduino Uno	A4	A5
+// Arduino Nano	A4	A5
+// Arduino Micro	2	3
+// Arduino Mega 2560	20	21
+// Arduino Leonardo	2	3
+// Arduino Due	20	21
 #define DISPLAY_I2C true
